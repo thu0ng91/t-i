42587af1a4ec2a7c24fec21cff4e6b588e3a6353
go123 <cz2051@163.com>
2014-07-26 22:54:02 +0800

    block sql

--- modules/block/data/block.sql
+++ modules/block/data/block.sql
@@ -1,11 +1,13 @@
+DROP TABLE IF EXISTS `block`;
 CREATE TABLE `block` (
   `bid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `blockname` varchar(50) NOT NULL DEFAULT '',
   `content` mediumtext NOT NULL,
+  `template` mediumtext NOT NULL COMMENT '外层模板',
   `vars` varchar(255) NOT NULL,
   `blocktype` varchar(10) NOT NULL DEFAULT '0',
   `sequence` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `status` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `cachetime` int(10) NOT NULL,
   PRIMARY KEY (`bid`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;

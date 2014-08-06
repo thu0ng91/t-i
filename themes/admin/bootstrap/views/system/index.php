b3cda62cec57674a49e229a4638abe4f3cc91167
go123 <cz2051@163.com>
2014-08-06 23:59:47 +0800

    admincp index

--- themes/admin/bootstrap/views/system/index.php
+++ themes/admin/bootstrap/views/system/index.php
@@ -5,23 +5,34 @@ $this->pageTitle=Yii::app()->name;
 ?>
 
 <?php $this->beginWidget('bootstrap.widgets.TbHeroUnit', array(
-    'heading'=> Yii::app()->name,
+    //'heading'=> Yii::app()->name,
 )); ?>
 
 <?php //$this->widget('bootstrap.widgets.TbLabel', array(
 //        'type'=>'success', // 'success', 'warning', 'important', 'info' or 'inverse'
 //        'label'=> Yii::app()->user->name,
 //    )); ?><!-- -->
-<p><?php echo Yii::app()->user->name;?>,您好！欢迎使用<?php echo Yii::app()->name;?> ,开源、免费、强大的小说系统！<br /> 当前系统版本：<b><?php echo FWXSVersion;?></b><br />我们的官方网站：<a href="http://www.yunyuewang.com" target="_blank">http://www.yunyuewang.com/</a></p>
-
-<p><?php $this->widget('bootstrap.widgets.TbButton', array(
+<p><?php echo Yii::app()->user->name;?>,您好！欢迎使用<?php echo Yii::app()->name;?> ,免费、强大的小说系统！<br /> 当前系统版本：<b><?php echo FWXSVersion;?></b> &nbsp;<?php $this->widget('bootstrap.widgets.TbButton', array(
         'type' => 'primary',
         'size' => 'normal',
-        'label' => '获取更多帮助',
+        'label' => '获取帮助',
         'url' => 'http://www.yunyuewang.com/bbs',
         'htmlOptions' => array('target' => '_blank'),
     )); ?></p>
-<div>
+
 	<iframe style="border:none;" src="http://www.yunyuewang.com/index.php?r=yunyue/iframenotice" width="500" height="200"></iframe>
-</div>
+
+<style>
+.lineheight{padding-left:5px;line-height:25px;height:25px;}
+</style>
+	<table class="tb tb2 fixpadding" style="font-size:12px;">
+<tbody><tr><th colspan="3" style="font-size:14px; font-weight:bold;text-align:left;">云阅开发团队</th></tr>
+<tr><td class="lineheight">架构设计：</td><td class="lineheight">pizigou</td></tr>
+<tr><td class="lineheight">功能规划：</td><td class="lineheight">go123</td></tr>
+<tr><td class="lineheight">产品研发：</td><td class="lineheight">yacms 冷雨夜</td></tr>
+<tr><td class="lineheight">界面设计：</td><td class="lineheight">阳阳/mg</td></tr>
+<tr><td class="lineheight">相关链接</td><td class="lineheight">
+		<a href="http://www.yunyuewang.com" class="lightlink2" target="_blank">公司网站</a>,
+		<a href="http://www.yunyuewang.com/bbs" class="lightlink2" target="_blank">讨论区</a></td></tr></tbody></table>
+
 <?php $this->endWidget(); ?>

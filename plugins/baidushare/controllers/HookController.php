ff2f574ad3ce9b7575839aa209f686035ec43968
go123 <cz2051@163.com>
2014-08-14 15:46:34 +0800

    baidushare

--- plugins/baidushare/controllers/HookController.php
+++ plugins/baidushare/controllers/HookController.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: think
- * Date: 14-7-5
- * Time: 下午4:23
- * To change this template use File | Settings | File Templates.
- */
 
 class HookController extends FWPluginFrontController {
 

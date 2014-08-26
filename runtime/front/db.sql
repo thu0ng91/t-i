d46c5545978ad5e0a3f1346ba0135914b6378b19
go123 <cz2051@163.com>
2014-08-26 22:11:50 +0800

    admincp page show nums

--- runtime/front/db.sql
+++ runtime/front/db.sql
@@ -223,3 +223,17 @@ CREATE TABLE `bookcase` (
   `readchaptertitle` varchar(100) NOT NULL COMMENT '最后阅读章节标题',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `searchlog`
+-- ----------------------------
+DROP TABLE IF EXISTS `searchlog`;
+CREATE TABLE `searchlog` (
+  `id` int(8) unsigned NOT NULL AUTO_INCREMENT,
+  `keywords` varchar(25) NOT NULL COMMENT '搜索关键词',
+  `nums` int(8) unsigned NOT NULL,
+  `result_nums` int(4) unsigned NOT NULL,
+  `lasttime` int(11) unsigned NOT NULL COMMENT '最后搜索时间',
+  `dateline` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;

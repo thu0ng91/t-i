b21e2e70842253b8768653e326e934a1fb590887
pizigou <pizigou@yeah.net>
2014-08-29 22:21:13 +0800

    1. 恢复被误删除的数据库配置文件

---
+++ runtime/front/db.config.php
@@ -0,0 +1,14 @@
+<?php 
+ return array (
+  'class' => 'CDbConnection',
+  'connectionString' => 'mysql:host=localhost;dbname=book',
+  'emulatePrepare' => true,
+  'username' => 'root',
+  'password' => 'root',
+  'charset' => 'utf8',
+  'tablePrefix' => '',
+  'enableProfiling' => true,
+  'enableParamLogging' => true,
+  'schemaCachingDuration' => 0,
+);
+?>
